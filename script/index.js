68b871d41228c21f151b127f42bbc473c43c6d27
Javiiz12 <paspezagenzy@gmail.com>
2023-02-02 15:51:11 +0100

    Initial commit

--- script/index.js
+++ script/index.js
@@ -1,18 +1,28 @@
-// Selecciona todos los productos
-const products = document.querySelectorAll(".product");
+const input = document.querySelector('input[type="search"]');
+const products = document.querySelectorAll('.product');
 
-// Agrega un evento de mouseover a cada producto
 products.forEach(product => {
   product.addEventListener("mouseover", function() {
-    // Aumenta el tamaño del producto en un 20%
     product.style.transform = "scale(1.2)";
   });
 });
 
-// Agrega un evento de mouseout a cada producto
 products.forEach(product => {
   product.addEventListener("mouseout", function() {
-    // Regresa el tamaño del producto a su tamaño original
     product.style.transform = "scale(1)";
+  });
+});
+
+input.addEventListener('keyup', function() {
+  const searchTerm = this.value.toLowerCase();
+
+  products.forEach(function(product) {
+    const title = product.querySelector('h2').textContent.toLowerCase();
+
+    if (title.indexOf(searchTerm) !== -1) {
+      product.style.display = 'block';
+    } else {
+      product.style.display = 'none';
+    }
   });
 });
